01f8f22dde505e235dc2d8b7b11fc1713e48fc17
Mark Porostosky Jr <markporo@yahoo.com>
2024-08-30 02:50:05 -0400

    blurred photos base 64

--- components/Releases.js
+++ components/Releases.js
@@ -14,6 +14,8 @@ const PreviousReleases = releases.slice(1, 5).map((release, i) => (
                 layout='responsive'
                 width={parseInt(release.width, 10)}
                 height={parseInt(release.height, 10)}
+                placeholder="blur"
+                blurDataURL={release.blurPhoto}
             />
         </a>
     </div>
@@ -28,6 +30,8 @@ const PreviousOlderReleases = releases.slice(5).map((release, i) => (
                 layout='responsive'
                 width={parseInt(release.width, 10)}
                 height={parseInt(release.height, 10)}
+                placeholder="blur"
+                blurDataURL={release.blurPhoto}
             />
         </a>
     </div>
@@ -40,7 +44,7 @@ export default function Releases() {
             <div className={listenStyles.featureReleaseWrapper}>
                 <div className={listenStyles.featureRelease}>
                     <a href={releases[0].link} target="_blank" rel="noopener noreferrer">
-                        <Image key='first and feature image' src={releases[0].src} alt={releases[0].alt} layout='responsive' priority width={releases[0].width} height={releases[0].height}></Image>
+                        <Image key='first and feature image' src={releases[0].src} alt={releases[0].alt} placeholder="blur" blurDataURL={releases[0].blurPhoto} layout='responsive' priority width={releases[0].width} height={releases[0].height}></Image>
                     </a>
                 </div>
             </div>
